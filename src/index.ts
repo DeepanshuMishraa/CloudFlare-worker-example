5b21acbe1a3ac2c4879d21503dd43444e64ac63c
Deepanshu Mishra <d4deepanshu723@gmail.com>
2024-11-17 00:26:54 +0530

    init1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		console.log(request.body);
+		console.log(request.headers);
+        console.log(request.method);
+        var uri = request.url.replace('/^https?:\/\//i', '/');
+
+		if (request.method === "GET") {
+            if(uri == "/users"){
+                //handle the routes for  /users
+            }
+			return Response.json({
+				message: "you sent a get request"
+			});
+		} else {
+			return Response.json({
+				message: "you did not send a get request"
+			});
+		}
 	},
-} satisfies ExportedHandler<Env>;
+};
